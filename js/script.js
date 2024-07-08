11a753b3f0d456ecdd7d45c4c0845c7cfd54c309
sebastien mukombe <sebastienmukombe@gmail.com>
2024-07-08 14:07:37 -0400

    la neivieme modification de cet code

--- js/script.js
+++ js/script.js
@@ -117,4 +117,35 @@ var swiper = new Swiper(".featured-slider",{
 }
 })
 
+var swiper = new Swiper(".reviews-slider",{
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop:true,
+  grabCursor:true,
+  centeredSlides:true,
+  autoplay:{
+    delay: 9500,
+    disableOnInteraction: false,
+
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+     0:{
+      slidesPerView: 1,
+    
+     },
+     768:{
+      slidesPerView: 2,
+   
+  },
+  991: {
+    slidesPerView: 3,
+   
+  },
+}
+})
+
 
